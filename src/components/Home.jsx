d9eee2d938ee52a02ef1da4c55c4115fb9a3eefc
deyink <yusufadeyinka55@gmail.com>
2024-02-02 01:30:00 +0100

    first commit

---
+++ src/components/Home.jsx
@@ -0,0 +1,87 @@
+import React from 'react'
+import background from "../assets/Hero_img.jpg"
+import "./css/Home.css"
+
+
+export default function Home() {
+
+
+    // var displayContent =  ()=>{
+    //   const pElement = document.getElementById('content')
+
+    //   var index = 0
+
+    //   const updatecontent = ()=>{
+    //     pElement.textContent = contentArray[index]
+
+    //   index = (index  + 1) % contentArray.length
+    //     setTimeout(updatecontent, 2000);
+    //   }
+    //   updatecontent(); 
+    // } 
+
+    // window.onload = displayContent();
+    
+
+  // const displayContent = ()=>{
+  //     const pElement = document.getElementById("content").innerHTML
+
+  //     let index = 0
+
+  //    const interval = setInterval(()=>{
+  //       if( index < contentArray.length ){
+  //         pElement.textContent  = contentArray[index];
+            
+  //           index++ 
+            
+  //       } else {
+  //         clearInterval(interval);
+  //       }
+  //     }, 2000 );
+  //   }
+  //  window.onload= displayContent();
+
+  // const displayContent = ()=>{
+  //   const pElement = document.getElementById('content')
+
+  //   var index = 0
+
+  //   const updateContent = ()=>{
+  //     pElement.textContent = contentArray[index]
+
+  //     index = (index +1 ) % contentArray;
+
+  //     updateContent()
+
+  //     const interval = setInterval(updateContent, 2000);
+  //   }
+  // }
+  // window.onload = displayContent()
+
+    return (
+      <div>
+         <div className="hero-container" id='Home'
+         style= {{
+          height: "95vh",
+          width: "100%",
+          backgroundImage: `url(${background})` ,
+          backgroundPosition: "center",
+          backgroundSize: "cover",
+          backgroundAttachment: "fixed",
+          backgroundRepeat: "no-repeat",
+               }}>
+              <div className="hero-name">
+                <h1>I'm Yusuf Adeyinka </h1>
+              <p id="content"></p>
+              </div>
+                
+         </div>
+     
+      </div>
+    )
+
+    
+  }
+
+
+
