4f3fb5f710c557d23b7dcc1620eb19ccb6cadc20
deyink <yusufadeyinka55@gmail.com>
2024-02-02 15:52:23 +0100

    image-compress

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -6,58 +6,6 @@ import "./css/Home.css"
 export default function Home() {
 
 
-    // var displayContent =  ()=>{
-    //   const pElement = document.getElementById('content')
-
-    //   var index = 0
-
-    //   const updatecontent = ()=>{
-    //     pElement.textContent = contentArray[index]
-
-    //   index = (index  + 1) % contentArray.length
-    //     setTimeout(updatecontent, 2000);
-    //   }
-    //   updatecontent(); 
-    // } 
-
-    // window.onload = displayContent();
-    
-
-  // const displayContent = ()=>{
-  //     const pElement = document.getElementById("content").innerHTML
-
-  //     let index = 0
-
-  //    const interval = setInterval(()=>{
-  //       if( index < contentArray.length ){
-  //         pElement.textContent  = contentArray[index];
-            
-  //           index++ 
-            
-  //       } else {
-  //         clearInterval(interval);
-  //       }
-  //     }, 2000 );
-  //   }
-  //  window.onload= displayContent();
-
-  // const displayContent = ()=>{
-  //   const pElement = document.getElementById('content')
-
-  //   var index = 0
-
-  //   const updateContent = ()=>{
-  //     pElement.textContent = contentArray[index]
-
-  //     index = (index +1 ) % contentArray;
-
-  //     updateContent()
-
-  //     const interval = setInterval(updateContent, 2000);
-  //   }
-  // }
-  // window.onload = displayContent()
-
     return (
       <div>
          <div className="hero-container" id='Home'
