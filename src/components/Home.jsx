e2b0d00acaa55b9a89b6265d1653fa4817029cde
Yusuf <yusufadeyinka55@gmail.com>
2024-03-21 10:02:35 +0100

    animation

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -19,10 +19,13 @@ export default function Home() {
                }}>
               <div className="hero-name">
                 <h1>I'm Yusuf Adeyinka </h1>
-              <h4>A Frontend web Developer</h4>
+                <div className="ido">
+                <h4>A Frontend web Developer</h4>
               <h4>A programmer</h4>
               <h4>A Web Designer</h4>
               
+                </div>
+           
               </div>
                 
          </div>
