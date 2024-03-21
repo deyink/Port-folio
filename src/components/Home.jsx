504f89b8a5586d01da351c939aec61184fdde7e4
Yusuf <yusufadeyinka55@gmail.com>
2024-03-21 08:56:58 +0100

    font fix

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -20,6 +20,7 @@ export default function Home() {
               <div className="hero-name">
                 <h1>I'm Yusuf Adeyinka </h1>
               <h4>A Frontend web Developer</h4>
+              <h4>A programmer</h4>
               <h4>A Web Designer</h4>
               
               </div>
